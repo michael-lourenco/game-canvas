9a787c6fb62bd5468b059be288464c16a5c05974
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-03-28 09:12:02 -0300

    Setup Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# HTML5 Canvas and JavaScript Game
+
+Based on video [Chris Courses](https://www.youtube.com/watch?v=eI9idPTT0c4)
---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvas');
+
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+console.log(canvas);
