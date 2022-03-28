18a8b5dd374c9daaccab12aee92982bcfac20900
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-03-28 12:51:54 -0300

    Create Enemies

--- index.js
+++ index.js
@@ -43,20 +43,80 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue');
 
-
 const projectiles = [];
+
+const enemies = [];
+
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 4);
+        let x;
+        let y;
+
+        if(Math.random() < 0.5){
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+            y = Math.random() * canvas.height;
+        } else {
+            x = Math.random() * canvas.width;
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+        }
+
+        const angleToCenter = Math.atan2(
+            canvas.height / 2 -y, 
+            canvas.width / 2 -x
+        ); 
+
+        const velocity = {
+            x: Math.cos(angleToCenter) * 5,
+            y: Math.sin(angleToCenter) * 5
+        }
+    
+        const enemy = new Enemy(x, y, radius, 'green', velocity);
+        enemies.push(enemy);
+        console.log(enemy)
+    }, 1000);
+}
+
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.draw();
+
     projectiles.forEach((projectile) => {
         projectile.update();
+    })
+
+    enemies.forEach((enemy) => {
+        enemy.update();
     })
 }
 
@@ -82,3 +142,4 @@ window.addEventListener('click', (event) =>{
 })
 
 animate();
+spawnEnemies();
