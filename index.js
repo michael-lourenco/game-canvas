794226f922a5448327a21dbbc5eafb5341c1463f
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-03-28 09:20:52 -0300

    Create Projectiles

--- index.js
+++ index.js
@@ -20,9 +20,32 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 20, 'blue');
 player.draw();
+
+window.addEventListener('click', (event) =>{
+    console.log(event.clientX, event.clientY);
+    const projectile = new Projectile(event.clientX, event.clientY, 5, 'red', null);
+    projectile.draw();
+})
 
