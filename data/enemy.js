7b55c35aeef552fefb2954609788df3349f75466
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-04-01 14:59:04 -0300

    Create new enemies

--- data/enemy.js
+++ data/enemy.js
@@ -1,10 +1,32 @@
-const dataEnemy =[{
-    name: 'Globin',
-    hp: 1,
-    attack: 1,
-    color: 'hsla(120,100%,50%,0.3)',
-    value: 1,
-    xp: 1,
-}] 
+const dataEnemy =
+{
+    0:{
+        name: 'Globin',
+        hp: 1,
+        attack: 1,
+        color: 'hsla(120,100%,50%,0.3)',
+        value: 1,
+        xp: 1,
+        radius: 20
+    },
+    1: {
+        name: 'Soldier',
+        hp: 2,
+        attack: 1,
+        color: 'hsla(240,100%,50%,0.3)',
+        value: 2,
+        xp: 3,
+        radius: 30
+    },
+    2: {
+        name: 'Captain',
+        hp: 5,
+        attack: 3,
+        color: 'hsla(300,100%,50%,0.3)',
+        value: 5,
+        xp: 15,
+        radius: 40
+    }
+}
 
 export { dataEnemy };
