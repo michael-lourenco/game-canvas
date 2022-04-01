d894ba6514c16bb51ef806325c81701f89f590df
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-04-01 13:41:23 -0300

    Add attributes and functions to Enemy

--- core/Enemy.js
+++ core/Enemy.js
@@ -1,12 +1,25 @@
 
 class Enemy {
-    constructor(context, x, y, radius, color, velocity) {
+    constructor(context, x, y, radius, velocity, { color, name, hp, attack, value, xp }) {
         this.context = context;
+        
         this.x = x;
         this.y = y;
+
         this.radius = radius;
         this.color = color;
+
         this.velocity = velocity;
+
+        this.name = name;
+
+        this.maxHp = hp;
+        this.currentHp  = hp;
+        
+        this.attack = attack;
+
+        this.value = value;
+        this.xp = xp;
     }
 
     draw() {
@@ -20,6 +33,18 @@ class Enemy {
         this.draw();
         this.x = this.x + this.velocity.x;
         this.y = this.y + this.velocity.y;
+    }
+
+    isDead() {
+        return this.currentHp <= 0;
+    }
+
+    takeDamage(damage) {
+        this.currentHp = this.currentHp - damage;
+    }
+
+    doDamage() {
+        return this.attack;
     }
 }
 
