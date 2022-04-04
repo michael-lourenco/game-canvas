50b3a2967f1c9ea61bea695369ce40bddf68b21c
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-04-04 18:42:31 -0300

    Give attributes for Projectile

--- core/Projectile.js
+++ core/Projectile.js
@@ -1,11 +1,25 @@
 class Projectile {
-    constructor(context, x, y, radius, color, velocity) {
+    constructor(context, x, y, velocity, { 
+    name,
+    pierce,
+    attack,
+    color,
+    cooldown,
+    radius,
+    }) {
         this.context = context;
         this.x = x;
         this.y = y;
-        this.radius = radius;
-        this.color = color;
+
         this.velocity = velocity;
+
+        this.attack = attack;
+        this.color = color;
+        this.cooldown = cooldown;
+        this.name = name;
+        this.pierce = pierce;
+        this.radius = radius;
+
     }
 
     draw() {
@@ -19,6 +33,10 @@ class Projectile {
         this.draw();
         this.x = this.x + this.velocity.x;
         this.y = this.y + this.velocity.y;
+    }
+
+    doDamage() {
+        return this.attack;
     }
 }
 
