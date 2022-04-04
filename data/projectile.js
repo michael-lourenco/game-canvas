693050be794acab78c7729d1f05feba04023e2e1
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-04-04 18:42:14 -0300

    Create models of projectile

---
+++ data/projectile.js
@@ -0,0 +1,23 @@
+const dataProjectile =
+{
+    0:{
+        name: 'gun',
+        pierce: 0,
+        attack: 1,
+        color: 'hsla(360,100%,50%,0.3)',
+        cooldown: 1,
+        radius: 10,
+        velocity_factor: 8,
+    },
+    1: {
+        name: 'riffle',
+        pierce: 1,
+        attack: 5,
+        color: 'hsla(240,100%,50%,0.3)',
+        cooldown: 5,
+        radius: 10,
+        velocity_factor: 30,
+    }
+}
+
+export { dataProjectile };
